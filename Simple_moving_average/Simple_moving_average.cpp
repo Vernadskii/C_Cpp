c9ee1f347df674511e04403702f5b85eeed53fe6
VernadskyDanya <d.vernadsky@yandex.ru>
2021-01-24 17:00:04 +0300

    feat: calculating last sum

--- Simple_moving_average/Simple_moving_average.cpp
+++ Simple_moving_average/Simple_moving_average.cpp
@@ -37,7 +37,7 @@ template < typename T >
 vector <T> SMA(const vector<T>* indication, const int n, char16_t window)
 {
 	vector <T> result_v;
-	result_v.reserve(n/window +1);
+	result_v.reserve(n/window + 1);
 	int count = 0;
 	T sum = 0;
 	for (auto element : *indication)
@@ -46,21 +46,27 @@ vector <T> SMA(const vector<T>* indication, const int n, char16_t window)
 		count++;
 		if (count == window)
 		{
-			result_v.push_back(sum/ static_cast<T>(window));
+			result_v.push_back(sum / static_cast<T>(window));
 			sum = 0;
 			count = 0;
 		}
+	}
+	if (count != 0) // Заносим последнюю сумму, членов которой не хватило до полного окна
+	{
+		result_v.push_back(sum / static_cast<T>(window));
 	}
 	return result_v;
 }
 
 int main()
 {
+	
 	int quantity = 1* 1000;
+	int window = 16;
 	vector <double> data;
 	data.reserve(quantity);
 	create_data<double>(quantity/1000, -100, 100, &data);
-	vector <double> result = SMA<double>(&data, quantity, 16);
+	vector <double> result = SMA<double>(&data, quantity, window);
 	print_vector(&result);
 	return 0;
 }
