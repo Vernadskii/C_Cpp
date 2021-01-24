0a52e042437ba855982d9a671b18b3053b55513e
VernadskyDanya <d.vernadsky@yandex.ru>
2021-01-24 17:09:57 +0300

    fix: was fixed empty result with quantity < 1000 and was added sum of last elements

--- Simple_moving_average/Simple_moving_average.cpp
+++ Simple_moving_average/Simple_moving_average.cpp
@@ -17,14 +17,14 @@ void print_vector(vector <T>* empty_data)
 
 
 /* Функция для генерации случайных тиков
-	quantity - количество тиков в тысячах, LO и HI - левая и правая границы рандома,
+	quantity - количество тиков, LO и HI - левая и правая границы рандома,
 	empty_data - пустой вектор для заполнения*/
 template < typename T >
 void create_data(const int& quantity, const int& LO, const int& HI, vector <T> * empty_data)
 {
 	srand(static_cast<unsigned int> (time(NULL)));
 	T rand_number = 0;
-	for (auto i = 0; i < quantity * 1000; i++)
+	for (auto i = 0; i < quantity; i++)
 	{
 		empty_data->push_back(LO + static_cast <T> (rand()) / (static_cast <T> (RAND_MAX / (HI - LO))));
 	}
@@ -51,21 +51,21 @@ vector <T> SMA(const vector<T>* indication, const int n, char16_t window)
 			count = 0;
 		}
 	}
-	if (count != 0) // Заносим последнюю сумму, членов которой не хватило до полного окна
+	if (count != 0) // Заносим последнюю сумму, членов которой не хватило до полного окна и сообщаем об этом
 	{
 		result_v.push_back(sum / static_cast<T>(window));
+		cout << "Last element in result didn't have enought ticks" << endl;
 	}
 	return result_v;
 }
 
 int main()
 {
-	
-	int quantity = 1* 1000;
-	int window = 16;
+	int quantity = 19;
+	int window = 8;
 	vector <double> data;
 	data.reserve(quantity);
-	create_data<double>(quantity/1000, -100, 100, &data);
+	create_data<double>(quantity, -100, 100, &data);
 	vector <double> result = SMA<double>(&data, quantity, window);
 	print_vector(&result);
 	return 0;
